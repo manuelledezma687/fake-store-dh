084d16025f0ccd4ef5ed44b6c6f293978c4cfd3d
Manuel Ledezma <manuelledezma687@gmail.com>
2023-12-16 13:16:23 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Obtener un botón y agregar un evento de clic
+const miBoton = document.querySelector("#efecto-boton");
+
+miBoton.addEventListener("click", function() {
+    // Realizar alguna acción cuando se hace clic en el botón
+    alert("¡Se hizo clic en el botón!");
+});
